aafd01f24d434f5a9752e34e5bc809d11fa1cb5e
Alexey Kostin <alexey-kostin@yandex.ru>
2022-08-25 16:10:05 +0300

    Updates

---
+++ index.0ac5eb89.js
@@ -0,0 +1,2 @@
+"https:"!==location.protocol&&(location.protocol="https:");document.querySelector(".footer__button_back").addEventListener("click",(()=>{window.history.back(),console.log("Going back")}));
+//# sourceMappingURL=index.0ac5eb89.js.map
---
+++ index.94e756ad.js
@@ -0,0 +1,2 @@
+!function(){"https:"!==location.protocol&&(location.protocol="https:");document.querySelector(".footer__button_back").addEventListener("click",(()=>{window.history.back(),console.log("Going back")}))}();
+//# sourceMappingURL=index.94e756ad.js.map
